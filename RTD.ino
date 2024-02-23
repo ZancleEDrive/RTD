a7855de24cbf47a046438947e435e5f3687b3a3b
iattaman <fabio.orbo@yahoo.it>
2024-02-23 14:50:00 +0100

    while 1

--- RTD.ino
+++ RTD.ino
@@ -1,28 +1,19 @@
 #include <mcp_can.h>
-#include <mcp_can_dfs.h>
 #include <SPI.h>
-
 #define LED 2
 #define BUTTON 3
 
-
-//ciao prova
-// CAN RX Variables
 long unsigned rxId;
 unsigned char len;
 byte rxBuf[8];
 unsigned int error = 0;
-//long unsigned int rxId2;
-//unsigned char len2;
-//byte rxBuf2[8];
-//boolean brake = false;
-//boolean TS = false;
-//boolean button_pressed = false;
-// CAN0 INT and CS
 MCP_CAN CAN0(10);   // Set CS to pin 10
 
 
-void setup() {
+
+int main (){
+  init();
+
   Serial.begin(115200);
   if (CAN0.begin(MCP_STDEXT, CAN_1000KBPS, MCP_8MHZ) == CAN_OK)
     Serial.println("MCP2515 Initialized Successfully!");
@@ -46,69 +37,28 @@ void setup() {
 
   pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
-}
 
-void loop() {
-do{
-    if (digitalRead(BUTTON) == 0){
-      Serial.println("Button Pressed");
-      CAN0.readMsgBuf(&rxId, &len, rxBuf);
-      if((rxId & 0xF1901) == 0xF1901){
-        error = 1;
+  while (1){
+    while (!error){
+      if (digitalRead(BUTTON) == 0){
+        Serial.println("Button Pressed");
+        CAN0.readMsgBuf(&rxId, &len, rxBuf);
+        if((rxId & 0x1FFFFFFF) == 0xF1901){
+          error = 1;
+          }
+        else if ((rxId & 0x1FFFFFFF) == 0xF1906){
+         //Turn on LED   
+          digitalWrite(LED, HIGH);
+          delay(2000);
+          //Turn off LED   
+          digitalWrite(LED, LOW);
+        //CAN0.readMsgBuf(&rxId, &len, rxBuf);
+        //CAN0.readMsgBuf(&rxId, &len, rxBuf);
+
         }
-      else if ((rxId & 0xF1906) == 0xF1906){
-       //Turn on LED   
-        digitalWrite(LED, HIGH);
-        delay(2000);
-        //Turn off LED   
-        digitalWrite(LED, LOW);
       }
-    }
-  } while (!error);
-}
-
-
-
-
-    
-  /*}
-  //Get a message
-  CAN0.readMsgBuf(&rxId, &len, rxBuf);
+  }
+        rxId = 0;
+ }  
   
-
-   if((rxId & 0xF1906) == 0xF1906){
-     if(rxBuf[0]!=0){
-       brake = true;
-       Serial.println("Brake pressed");
-     }
-   }
-   
-  /*
-   * TS CHECK
-   
-   else if((rxId & 0xF1901) == 0xF1901){
-    if(rxBuf[0]!=0){
-      Serial.println("TS ACTIVE");
-      TS = true;
-    }
-    else{
-      TS = false;
-    }
-   }
-
-   //Overall check
-   if(TS && brake && button_pressed){
-     //Turn on LED   
-      digitalWrite(LED, HIGH);
-      delay(2000);
-      //Turn off LED   
-      digitalWrite(LED, LOW);
-
-      //RESET
-      TS = false;
-      brake = false;
-      button_pressed = false;
-      rxId = 0;
-   }
-
-}*/
+}
